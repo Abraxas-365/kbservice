e4f6e95383f8483c17b6849036b98da3400ab62a
Abraxas-365 <luisfmiranda8@gmail.com>
2025-02-09 17:12:38 +0700

    fix: deleting un used options

--- kb/options.go
+++ kb/options.go
@@ -10,10 +10,6 @@ type Options struct {
 	Namespace      string
 	ScoreThreshold float32
 	Filters        vectorstore.Filter
-	IndexName      string
-	Dimensions     int
-	Distance       vectorstore.DistanceMetric
-	TopK           int
 	LLM            *llm.LLM // Optional LLM
 }
 
@@ -24,8 +20,6 @@ type Option func(*Options)
 func defaultOptions() *Options {
 	return &Options{
 		ScoreThreshold: 0.0,
-		Distance:       vectorstore.Cosine,
-		TopK:           4,
 		LLM:            nil, // Default to no LLM
 	}
 }
@@ -41,34 +35,6 @@ func WithScoreThreshold(threshold float32) Option {
 func WithFilters(filters vectorstore.Filter) Option {
 	return func(o *Options) {
 		o.Filters = filters
-	}
-}
-
-// WithIndexName sets the index name
-func WithIndexName(indexName string) Option {
-	return func(o *Options) {
-		o.IndexName = indexName
-	}
-}
-
-// WithDimensions sets the vector dimensions
-func WithDimensions(dimensions int) Option {
-	return func(o *Options) {
-		o.Dimensions = dimensions
-	}
-}
-
-// WithDistanceMetric sets the distance calculation method
-func WithDistanceMetric(metric vectorstore.DistanceMetric) Option {
-	return func(o *Options) {
-		o.Distance = metric
-	}
-}
-
-// WithTopK sets the number of similar documents to retrieve
-func WithTopK(k int) Option {
-	return func(o *Options) {
-		o.TopK = k
 	}
 }
 
